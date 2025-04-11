891443199dab4c0cd824738bbac5a65e16b6bb8b
muggloaf <aleshamulla2006@gmail.com>
2025-04-11 14:50:50 +0530

    alesha's first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,14 @@
 // ignore_for_file: avoid_print
 
 import 'package:flutter/material.dart';
+
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-  
-  
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
@@ -16,7 +16,8 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.orange, primary: Colors.orange),
+        colorScheme: ColorScheme.fromSeed(
+            seedColor: Colors.orange, primary: Colors.orange),
         scaffoldBackgroundColor: Colors.white,
       ),
       home: MyHomePage(title: '...'),
@@ -42,7 +43,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   final String username = 'Test';
   String getGreetingTime() {
     final hour = DateTime.now().hour;
@@ -55,8 +55,22 @@ class _MyHomePageState extends State<MyHomePage> {
     }
   }
 
-  final List<IconData> icons = [Icons.contact_phone_rounded, Icons.event, Icons.abc, Icons.abc, Icons.abc, Icons.abc];
-  final List<String> titles = ['Contacts', 'Events', 'To-Do','Projects','Reminders','Notes'];
+  final List<IconData> icons = [
+    Icons.contact_phone_rounded,
+    Icons.event,
+    Icons.pending_actions_rounded,
+    Icons.abc,
+    Icons.abc,
+    Icons.abc
+  ];
+  final List<String> titles = [
+    'Contacts',
+    'Events',
+    'To-Do',
+    'Projects',
+    'Reminders',
+    'Notes'
+  ];
   @override
   Widget build(BuildContext context) {
     // This method is rerun every time setState is called, for instance as done
@@ -67,75 +81,79 @@ class _MyHomePageState extends State<MyHomePage> {
     // than having to individually change instances of widgets.
     final String greetingTime = getGreetingTime();
     return Scaffold(
-      appBar: AppBar(
-        flexibleSpace: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [Colors.deepOrange, Colors.orange], // Darker to lighter orange
-              begin: Alignment.topCenter,
-              end: Alignment.bottomCenter,
-              ),
-            border: Border(
-              bottom: BorderSide(
-                color: Colors.black,
-                width: 2.0, // Border width
-              ),
-            )
-          ),
-        ),
-        toolbarHeight: 100,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  'Good $greetingTime,',
-                  style: const TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize : 30,
-                    color : Colors.white, 
+        appBar: AppBar(
+          flexibleSpace: Container(
+            decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [
+                    Colors.deepOrange,
+                    Colors.orange
+                  ], // Darker to lighter orange
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                ),
+                border: Border(
+                  bottom: BorderSide(
+                    color: Colors.black,
+                    width: 2.0, // Border width
                   ),
-                ),
-                Text(
-                  username,
-                  style: const TextStyle(
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 35,
-                ),
-              )
+                )),
+          ),
+          toolbarHeight: 100,
+          // Here we take the value from the MyHomePage object that was created by
+          // the App.build method, and use it to set our appbar title.
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Good $greetingTime,',
+                    style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 30,
+                      color: Colors.white,
+                    ),
+                  ),
+                  Text(
+                    username,
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 35,
+                    ),
+                  )
+                ],
+              ),
+              IconButton(
+                icon: const Icon(Icons.settings, color: Colors.white, size: 35),
+                onPressed: () {
+                  print('Settings button pressed');
+                },
+              ),
             ],
           ),
-          
-          IconButton(
-              icon: const Icon(Icons.settings, color: Colors.white, size: 35),
-              onPressed: () {
-                print('Settings button pressed');
-              },
-            ),
-          ],
-        ), 
-      ), 
-      body: Center(
-        child: ListView.builder(
-          shrinkWrap: true,
-          itemCount: titles.length,
-          itemBuilder: (context, index) {
-            return Padding(
-              padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+        ),
+        body: Center(
+          child: ListView.builder(
+            shrinkWrap: true,
+            itemCount: titles.length,
+            itemBuilder: (context, index) {
+              return Padding(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
                 child: Container(
                   margin: const EdgeInsets.symmetric(horizontal: 25),
                   decoration: BoxDecoration(
                     color: Colors.orange.shade300,
                     borderRadius: BorderRadius.circular(10),
-
                   ),
                   child: ListTile(
-                    leading: Icon(icons[index], color: Colors.black,),
+                    leading: Icon(
+                      icons[index],
+                      color: Colors.black,
+                    ),
                     title: Center(
                       child: Text(
                         titles[index],
@@ -146,15 +164,13 @@ class _MyHomePageState extends State<MyHomePage> {
                       ),
                     ),
                     onTap: () {
-                      
                       print('Tapped on ${titles[index]}');
                     },
                   ),
-              ),
-            );
-          },
-        ), 
-      )
-    );
+                ),
+              );
+            },
+          ),
+        ));
   }
 }
